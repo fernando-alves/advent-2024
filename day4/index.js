276d915a26e9e22c992833a6dbfcbf19dd80e1f3
Fernando Junior <fernandoasfj+github@gmail.com>
2024-12-05 00:14:29 +0100

    replace line with row

--- day4/index.js
+++ day4/index.js
@@ -3,40 +3,40 @@ const fs = require('fs')
 const readFile = path => fs.readFileSync(path).toString()
 const inputLines = (separator = '\n') => readFile('input').split(separator)
 
-const xmasFrom = (line, column, board) => {
+const xmasFrom = (row, column, board) => {
   const possibilities = []
 
   if (column + 3 < board.length)
-    possibilities.push(`${board[line][column]}${board[line][column+1]}${board[line][column+2]}${board[line][column+3]}`)
+    possibilities.push(`${board[row][column]}${board[row][column+1]}${board[row][column+2]}${board[row][column+3]}`)
 
   if (column - 3 >= 0)
-    possibilities.push(`${board[line][column]}${board[line][column-1]}${board[line][column-2]}${board[line][column-3]}`)
+    possibilities.push(`${board[row][column]}${board[row][column-1]}${board[row][column-2]}${board[row][column-3]}`)
 
-  if (line + 3 < board.length)
-    possibilities.push(`${board[line][column]}${board[line+1][column]}${board[line+2][column]}${board[line+3][column]}`)
+  if (row + 3 < board.length)
+    possibilities.push(`${board[row][column]}${board[row+1][column]}${board[row+2][column]}${board[row+3][column]}`)
 
-  if (line - 3 >= 0 )
-    possibilities.push(`${board[line][column]}${board[line-1][column]}${board[line-2][column]}${board[line-3][column]}`)
+  if (row - 3 >= 0 )
+    possibilities.push(`${board[row][column]}${board[row-1][column]}${board[row-2][column]}${board[row-3][column]}`)
 
-  if (line + 3 < board.length && column + 3 < board.length)
-    possibilities.push(`${board[line][column]}${board[line+1][column+1]}${board[line+2][column+2]}${board[line+3][column+3]}`)
+  if (row + 3 < board.length && column + 3 < board.length)
+    possibilities.push(`${board[row][column]}${board[row+1][column+1]}${board[row+2][column+2]}${board[row+3][column+3]}`)
 
-  if (line - 3 >= 0 && column + 3 < board.length)
-    possibilities.push(`${board[line][column]}${board[line-1][column+1]}${board[line-2][column+2]}${board[line-3][column+3]}`)
+  if (row - 3 >= 0 && column + 3 < board.length)
+    possibilities.push(`${board[row][column]}${board[row-1][column+1]}${board[row-2][column+2]}${board[row-3][column+3]}`)
 
-  if (line + 3 < board.length && column - 3 >= 0)
-    possibilities.push(`${board[line][column]}${board[line+1][column-1]}${board[line+2][column-2]}${board[line+3][column-3]}`)
+  if (row + 3 < board.length && column - 3 >= 0)
+    possibilities.push(`${board[row][column]}${board[row+1][column-1]}${board[row+2][column-2]}${board[row+3][column-3]}`)
 
-  if (line - 3 >= 0 && column - 3 >= 0)
-    possibilities.push(`${board[line][column]}${board[line-1][column-1]}${board[line-2][column-2]}${board[line-3][column-3]}`)
+  if (row - 3 >= 0 && column - 3 >= 0)
+    possibilities.push(`${board[row][column]}${board[row-1][column-1]}${board[row-2][column-2]}${board[row-3][column-3]}`)
 
   return possibilities.filter(p => p === 'XMAS' || p === 'SAMX').length
 }
 
 const board = inputLines().map(line => line.split(''))
 
-const result = board.reduce((acc, line, lineIndex) =>
-  line.reduce((lineAcc, char, columnIndex) => char === 'X' ? lineAcc + xmasFrom(lineIndex, columnIndex, board) : lineAcc, acc)
+const result = board.reduce((acc, row, rowIndex) =>
+  row.reduce((rowAcc, char, columnIndex) => char === 'X' ? rowAcc + xmasFrom(rowIndex, columnIndex, board) : rowAcc, acc)
 , 0)
 
 console.log(result)
