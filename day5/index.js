1ea56d14a6fdb0a75221735e693893c1e7615362
Fernando Junior <fernandoasfj+github@gmail.com>
2024-12-07 14:04:09 +0100

    day5 part 2

--- day5/index.js
+++ day5/index.js
@@ -3,6 +3,12 @@ const fs = require('fs')
 const readFile = path => fs.readFileSync(path).toString()
 const inputLines = (separator = '\n') => readFile('input').split(separator)
 
+const rulesAndUpdates = lines => lines.reduce(({ rules, updates }, input) => {
+  if (input.includes('|')) rules = addRule(input, rules)
+  if (input.includes(',')) updates = addUpdate(input, updates)
+  return { rules, updates }
+}, { rules: {}, updates: [] })
+
 const addRule = (rule, rules) => {
   const [key, subsequent] = rule.split('|').map(Number)
   rules[key] = [...(rules[key] || []), subsequent]
@@ -13,13 +19,39 @@ const addUpdate = (update, updates) => [...updates, update.split(',').map(Number
 
 const isInRightOrder = (update, rules) => update.slice(1).every((entry, index) => update.slice(0, index + 1).every(p => !rules[entry]?.includes(p)))
 
-const { rules, updates } = inputLines().reduce(({ rules, updates }, input) => {
-  if (input.includes('|')) rules = addRule(input, rules)
-  if (input.includes(',')) updates = addUpdate(input, updates)
-  return { rules, updates }
-}, { rules: {}, updates: [] })
+const checkCorrectness = (updates, rules) => updates.reduce(({ correct, incorrect }, update) => {
+  isInRightOrder(update, rules) ? correct.push(update) : incorrect.push(update)
+  return { correct, incorrect }
+}, { correct: [], incorrect: [] })
 
-const correctUpdates = updates.filter(update => isInRightOrder(update, rules))
-const sumMiddlePage = correctUpdates.reduce((sum, update) => sum + update[Math.floor(update.length / 2)], 0)
+const sumMiddle = updates => updates.reduce((sum, update) => sum + update[Math.floor(update.length / 2)], 0)
 
-console.log(sumMiddlePage)
+const swap = (update, i, j) => {
+  const swapped = [...update]
+  swapped[i] = update[j]
+  swapped[j] = update[i]
+  return swapped
+}
+
+const fixUpdate = update => {
+  for(let i = 0; i < update.length; i++) {
+    const entry = update[i]
+    const defectivePrecedent = update.slice(0, i).findIndex(p => rules[entry]?.includes(p))
+
+    if (defectivePrecedent >= 0) {
+      return fixUpdate(swap(update, i, defectivePrecedent))
+    }
+  }
+
+  return update
+}
+
+const { updates, rules } = rulesAndUpdates(inputLines())
+
+const { correct, incorrect } = checkCorrectness(updates, rules)
+
+console.log(sumMiddle(correct))
+
+const fixed = incorrect.map(fixUpdate)
+
+console.log(sumMiddle(fixed))
